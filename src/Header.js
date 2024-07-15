0618ac299047f3ba1ebd3267017d1a9aec36294f
Wilfred Naraga <wilfrednaraga@Wilfreds-MacBook-Air.local>
2024-07-15 19:54:22 -0400

    initializing project

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+//Header.js
+
+import React from 'react';
+import './App.css';
+
+const Header = () => {
+    return (
+        <div className = "centered">
+            <h1>Manga King</h1>
+        </div>
+    );
+}
+
+export default Header;
