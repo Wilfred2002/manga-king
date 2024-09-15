9746495eff30c1ae741ccd2045cb96f5115510ff
Wilfred <wfnaraga@gmail.com>
2024-09-15 13:49:39 -0400

    Initialized repository

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+import Nav from "../../components/Nav";
+
+export default function About() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-300">
+      <Nav />
+      <div className="flex flex-col justify-center items-center p-8">
+        <h1 className="text-4xl font-bold mb-4 text-center">
+          About This Project
+        </h1>
+        <p className="text-lg max-w-2xl text-center">
+          This website was developed by Wilfred Naraga as an educational project.
+          Built on Typescript using NextJS and Tailwind CSS, 
+          it also uses the MangaDex API to provide the content..
+          The goal is to create a smooth experience for manga readers.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ModeToggle } from "./ui/ModeToggle";
+
+export default function Nav() {
+  return (
+    <nav>
+      <header className="flex justify-between items-center p-14">
+        <Link href="/">
+          <h1 className="text-3xl">Manga King</h1>
+        </Link>
+        <ul className="flex space-x-4 ml-auto">
+          <li>
+            <Link href="/">
+              <Button variant="ghost">Home</Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/top10">
+              <Button variant="ghost">Top 10 Weekly</Button>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <Button variant="ghost">About</Button>
+            </Link>
+          </li>
+          <ModeToggle />
+        </ul>
+      </header>
+    </nav>
+  );
+}
