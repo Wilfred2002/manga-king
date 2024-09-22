b744535abfb93abf7d3e228ae617492e75665265
Wilfred <wfnaraga@gmail.com>
2024-09-22 00:35:52 -0400

    revamped css styling for manga details page

--- app/manga/[id]/page.tsx
+++ app/manga/[id]/page.tsx
@@ -1,9 +1,22 @@
 "use client"; 
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+//importing table from shadcn
+
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import Nav from '@/components/Nav'; 
 import axios from 'axios';
+
 
 function MangaDetails() {
   const {id} = useParams();
@@ -11,11 +24,32 @@ function MangaDetails() {
   const[manga, setManga] = useState(null);
   const[author, setAuthor] = useState(null);
   const[authorsID, setauthorsID] = useState(null);
+  const[coverArt, setCoverArt] = useState(null);
+  const[mangaStatus, setmangaStatus] = useState(null);
+  const[mangaDemographic, setmangaDemographic] = useState(null);
+  const[mangaDescription, setmangaDescription] = useState(null);
+  const[mangaArtists, setmangaArtist] = useState(null);
+  const[mangaRating, setmangaRating] = useState(null);
+  const[mangaPublished, setmangaPublished] = useState(null);
+
+  //Setting useState 
+
+  const getCoverUrl = (manga) => {
+    const coverRelation = manga.relationships.find((rel) => rel.type === "cover_art");
+    if (coverRelation && coverRelation.attributes && coverRelation.attributes.fileName) {
+      // construct url using both manga.id and the fileName
+      return `https://uploads.mangadex.org/covers/${manga.id}/${coverRelation.attributes.fileName}.512.jpg`;
+    }
+    return null;
+  };
 
   async function fetchData(){
     try{
-      const response = await axios.get(`https://api.mangadex.org/manga/${id}`)
+      const response = await axios.get(`https://api.mangadex.org/manga/${id}?includes%5B%5D=cover_art&includes%5B%5D=author&includes%5B%5D=artist&includes%5B%5D=tag&includes%5B%5D=creator`)
       console.log(response.data); //logging json file 
+
+      const coverPic = getCoverUrl(response.data.data);
+      setCoverArt(coverPic);
 
       let title = response.data.data.attributes.title.en;
       if(!title){
@@ -31,15 +65,40 @@ function MangaDetails() {
       setauthorsID(authorID); //grabbing author's ID from first api call
 
       if(authorID){
-        const response2 = await axios.get(`https://api.mangadex.org/author/${authorID}`);
+        const response2 = await axios.get(`https://api.mangadex.org/author/${authorID}`); //second API call for author's name
         console.log(response2.data);
 
-        const authorName = response2.data.data.attributes.name;
+        const authorName = response2.data.data.attributes.name; //setting the name
         setAuthor(authorName);
+      } else {
+        console.error("No author found");
       }
 
+      const status = response.data.data.attributes.status;
+      setmangaStatus(status); //getting status of manga
 
-    
+      const demographic = response.data.data.attributes.publicationDemographic;
+      setmangaDemographic(demographic); //grabbing description of manga
+
+      const descript = response.data.data.attributes.description.en;
+      const trimmedDescription = descript.split('---')[0].trim();
+      setmangaDescription(trimmedDescription); // grabbing english description
+
+      const artist = response.data.data.relationships[1].attributes.name;
+      setmangaArtist(artist);
+
+      const year = response.data.data.attributes.year;
+      setmangaPublished(year);
+
+      //const response3 = await axios.get(`https://api.mangadex.org/rating?manga=`, {
+      //  params: { manga: [id] } // Passing the ID as an array
+      //});
+
+      //console.log(response3.data);
+      //const rating = response3.data.ratings.rating;
+      //setmangaRating(rating);
+
+
 
     } catch(error){
       console.error("error:", error);
@@ -51,11 +110,80 @@ function MangaDetails() {
   }, [id]);
 
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-300">
+    <div className="min-h-screen bg-white dark:bg-gray-800 text-black dark:text-gray-300 font-sans">
       <Nav />
-      <h1>{manga ? manga : "Loading title..."}</h1>
-      <h1>{mangaType ? mangaType : "Loading type..."}</h1>
-      <h1>{author ? author : "Loading author..."}</h1>
+      <div className = "flex flex-col md:flex-row py-8 max-w-screen-xl mx-auto"> 
+        <div className = "w-1/3">
+          <div>{coverArt ? <img src ={coverArt} alt = "manga cover"></img> : <p>No cover art available</p>}</div>
+        </div>
+        <div className = "w-2/3 px-6">
+
+          <h1 className = "text-4xl font-bold">{manga ? manga : "Loading title..."}</h1>
+          <div className = "border-t border-gray-600 my-4"></div>
+
+          <div>
+            <span className="text-gray-400">Origination: </span>
+            <span>{mangaType ? mangaType : "Loading type..."}</span>
+          </div>
+
+          <div>
+            <span className="text-gray-400">Demographic: </span>
+            <span>{mangaDemographic ? mangaDemographic : ""}</span>
+          </div>
+
+          <div>
+            <span className="text-gray-400">Published: </span>
+            <span>{mangaPublished ? mangaPublished : "Loading year..."}</span>
+          </div>
+
+          <div>
+            <span className="text-gray-400">Status: </span>
+            <span>{mangaStatus ? mangaStatus : "Loading manga status..."}</span>
+          </div>
+
+          <div>
+            <span className = "text-gray-400">Rating: </span>
+            <span>{mangaRating ? mangaRating : "Loading rating..."}</span>
+          </div>
+
+          <h1 className="text-2xl py-3 font-bold">Description</h1>
+          <p>{mangaDescription ? mangaDescription : "Loading description..."}</p>
+
+          <div className = "border-t border-gray-600 my-4"></div>
+
+          <h1 className = "text-2xl py-3 font-bold">More Info</h1>
+          
+          <div>
+            <span className="text-gray-400">Artist: </span>
+            <span>{mangaArtists ? mangaArtists : "Loading manga artists..."}</span>
+          </div>
+
+          <div>
+            <span className="text-gray-400">Author: </span>
+            <span>{author ? author : "Loading author..."}</span>
+          </div>
+
+        </div>
+      </div>
+
+      <div className = "flex flex-col md:flex-row py-8 max-w-screen-xl mx-auto">
+        <h1 className = "text-3xl font-bold">Chapters</h1>
+
+        <div>
+          <Table>
+            <TableHeader>
+            <TableRow>
+                <TableHead className="w-[100px]">Chapter</TableHead>
+                <TableHead>Uploaded</TableHead>
+                <TableHead>Group</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+          </Table>
+        </div>
+
+
+      </div>
 
     </div>
   );
