5142846795bd7209d273e185f6d4039a75d59342
Wilfred <wfnaraga@gmail.com>
2024-09-26 14:38:44 -0400

    debugging

---
+++ app/manga/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import Nav from '@/components/Nav';
+
+export default function MangaPage() {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>Manga Page</h1>
+        </div>
+    );
+}
