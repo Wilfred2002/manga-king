4e8c5d870baaff5c934bcd0363898e25667f1ff4
Wilfred <wfnaraga@gmail.com>
2024-09-27 15:03:46 -0400

    minor changes

--- app/manga/[id]/[chapterId]/page.tsx
+++ app/manga/[id]/[chapterId]/page.tsx
@@ -1,10 +1,7 @@
 import ReadChapter from "./ReadChapter";
 
-function generateStaticParams(){
-  return [];
-}
 
-export default function ReadChapterPage({ params }: { params: { id: string; chapterId: string } }) {
+function ReadChapterPage({ params }: { params: { id: string; chapterId: string } }) {
   const { id, chapterId } = params; 
 
   return <ReadChapter id={id} chapterId={chapterId} />;
