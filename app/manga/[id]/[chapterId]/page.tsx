7473a8de1e6ebd60c379d8397cdfc0a530847302
Wilfred <wfnaraga@gmail.com>
2024-09-27 15:01:55 -0400

    generate static params

--- app/manga/[id]/[chapterId]/page.tsx
+++ app/manga/[id]/[chapterId]/page.tsx
@@ -1,5 +1,8 @@
 import ReadChapter from "./ReadChapter";
 
+function generateStaticParams(){
+  return [];
+}
 
 export default function ReadChapterPage({ params }: { params: { id: string; chapterId: string } }) {
   const { id, chapterId } = params; 
