3e5c2481e956424cb06999690bab6534605e4da8
Wilfred <wfnaraga@gmail.com>
2024-09-27 22:54:45 -0400

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
     images: {
       domains: ['uploads.mangadex.org'], 
     },
   };
+
+  module.exports = nextConfig
   
   export default nextConfig;
   
